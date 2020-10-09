5b1afdff0db32ba6dc579315821bc5ffb6b31529
kelibst <kboster17@gmail.com>
2020-10-09 05:19:47 +0000

    add and style footer components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './components/layouts/Footer/Footer';
 import Header from './components/layouts/header/Header';
 import Movies from './components/layouts/Movies/Movies';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Movies />
+      <Footer />
     </div>
   );
 }
