dde45d7483d2f3c3c43b7ac8e23a63f5d38c9335
kelibst <kboster17@gmail.com>
2020-10-09 04:53:01 +0000

    implement bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
