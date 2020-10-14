0519f1760dfc6aad8cc9daeb4f63c8188f019d32
kelibst <kboster17@gmail.com>
2020-10-14 10:33:12 +0000

    add movie videos to detail page

---
+++ src/components/layouts/Movies/Iframer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Iframer = props => {
+    const {video} = props
+    console.log(video)
+    return (
+        <div className="col col-md-6 my-5">
+            <iframe src={`https://www.youtube.com/embed/${video.key}`} 
+            width="100%" height="100%" allowFullScreen="true" />
+        </div>
+    )
+}
+
+Iframer.propTypes = {
+
+}
+
+export default Iframer
--- src/components/pages/detailsPage.js
+++ src/components/pages/detailsPage.js
@@ -2,7 +2,9 @@
 import React, { Component } from 'react';
 import Icofont from 'react-icofont';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchMovie, fetchMovies } from '../../store/actions/fetchAction';
+import Iframer from '../layouts/Movies/Iframer';
 import url from '../layouts/Movies/urlGenerator';
 
 class detailsPage extends Component {
@@ -20,18 +22,35 @@ class detailsPage extends Component {
     console.log(data)
     const report = data ? (
       <div className="m-2 p-0 d-sm-flex border-0">
-      <button type="button" className="btn btn-transparent text-white btn-icon"  onClick={()=> this.props.history.goBack()}><Icofont icon="bubble-left" className="icon-dark iconStyle" /></button>
-        <div className="col-md-5 movie-img" style={{ backgroundImage: `url(${url(data.poster_path)})` }}><h6 className="text-danger re_date">{data.release_date}</h6></div>
-        <div className="movie-body col-md-7 text-white">
-          <h4 className="movie-title text-white">{data.title}</h4>
-          <div className="genres">
-            <h6>Genres</h6>
-            {data.genres && data.genres.map(genre => {
-              return (<button key={genre.id} className="btn btn-primary m-1 text-white">{genre.name}</button> )
-            })}
+        <button type="button" className="btn btn-transparent text-white btn-icon"  onClick={()=> this.props.history.goBack()}><Icofont icon="bubble-left" className="icon-dark iconStyle" /></button>
+          <div className="col-md-5 movie-img" style={{ backgroundImage: `url(${url(data.poster_path)})` }}>
+              <h6 className="text-danger re_date">{data.release_date}</h6> 
+              {data.homepage ? (<a href={data.homepage} target="_blank" className="btn btn-outline-success homepage text-white">Homepage</a> ) : ("") }</div>
+          <div className="movie-body col-md-7 text-white">
+            <h4 className="movie-title text-white">{data.title}</h4>
+            <div className="genres my-3">
+              <h6>Genres</h6>
+              {data.genres && data.genres.map(genre => {
+                return (<button key={genre.id} className="btn btn-primary m-1 text-white">{genre.name}</button> )
+              })}
+
+              {!data.genres ? (<p className="text-secondary">No Genre for this movie</p>) : ""}
+
+            </div>
+            <p className="text-white movie-overview">{data.overview}</p>
+
+           
+            <h4 className="text-white mb-3">Trailers</h4> 
+            <div className="videos d-sm-flex">
+                {data.videos.results && data.videos.results.map(video => {
+                  return (
+                    <Iframer video= {video} key={video.key} />
+                  )
+                })}
+            </div>
           </div>
-          <p className="text-white movie-overview">{data.overview}</p>
-        </div>
+
+        
       </div>
     ) : (
       <div className="container">
