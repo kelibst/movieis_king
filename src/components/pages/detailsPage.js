64c4f4a008436ffb8d2bee125dcb0da7511ba34f
Keli Booster <kbooster17@gmail.com>
2020-10-16 14:26:18 +0000

    fix linters

--- src/components/pages/detailsPage.js
+++ src/components/pages/detailsPage.js
@@ -1,8 +1,8 @@
-/* eslint-disable */
+/* eslint-disable  react/destructuring-assignment */
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Icofont from 'react-icofont';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import ErRors from '../../containers/ErRors';
 import Loading from '../../containers/Loading';
 import { fetchMovie, fetchMovies } from '../../store/actions/fetchAction';
@@ -12,52 +12,50 @@ import url from '../layouts/Movies/urlGenerator';
 class detailsPage extends Component {
   componentDidMount() {
     const id = this.props.match.params.movie_id;
-    const { movies } = this.props
-    if(!movies.length){
-      this.props.fetchMovies()
+    const { movies } = this.props;
+    if (!movies.length) {
+      this.props.fetchMovies();
     }
     this.props.fetchMovie(id);
   }
 
   render() {
     const { data } = this.props.movie;
-    const { errors } = this.props
-    console.log(data)
+    const { errors } = this.props;
     const report = data ? (
       <div className="m-2 p-0 d-sm-flex border-0">
-        <button type="button" className="btn btn-transparent text-white btn-icon"  onClick={()=> this.props.history.goBack()}><Icofont icon="bubble-left" className="icon-dark iconStyle" /></button>
-          <div className="col-md-5 movie-img" style={{ backgroundImage: `url(${url(data.poster_path)})` }}>
-              <h6 className="text-danger re_date">{data.release_date}</h6> 
-              {data.homepage ? (<a href={data.homepage} target="_blank" className="btn btn-outline-success homepage text-white">Homepage</a> ) : ("") }</div>
-          <div className="movie-body col-md-7 text-white">
-            <h4 className="movie-title text-white">{data.title}</h4>
-            <div className="genres my-3">
-              <h6>Genres</h6>
-              {data.genres && data.genres.map(genre => {
-                return (<button key={genre.id} className="btn btn-primary m-1 text-white">{genre.name}</button> )
-              })}
+        <button type="button" className="btn btn-transparent text-white btn-icon" onClick={() => this.props.history.goBack()}>
+          {' '}
+          <i className="d-none">Back</i>
+          <Icofont icon="bubble-left" className="icon-dark iconStyle" />
+        </button>
+        <div className="col-md-5 movie-img" style={{ backgroundImage: `url(${url(data.poster_path)})` }}>
+          <h6 className="text-danger re_date">{data.release_date}</h6>
+          {data.homepage ? (<a href={data.homepage} target="_blank" rel="noreferrer" className="btn btn-outline-success homepage text-white">Homepage</a>) : ('') }
+        </div>
+        <div className="movie-body col-md-7 text-white">
+          <h4 className="movie-title text-white">{data.title}</h4>
+          <div className="genres my-3">
+            <h6>Genres</h6>
+            {data.genres && data.genres.map(genre => (<button key={genre.id} type="button" className="btn btn-primary m-1 text-white">{genre.name}</button>))}
 
-              {!data.genres ? (<p className="text-secondary">No Genre for this movie</p>) : ""}
+            {!data.genres ? (<p className="text-secondary">No Genre for this movie</p>) : ''}
 
-            </div>
-            <p className="text-white movie-overview">{data.overview}</p>
-
-           
-            <h4 className="text-white mb-3">Trailers</h4> 
-            <div className="videos d-sm-flex">
-                {data.videos.results && data.videos.results.map(video => {
-                  return (
-                    <Iframer video= {video} key={video.key} />
-                  )
-                })}
-            </div>
           </div>
+          <p className="text-white movie-overview">{data.overview}</p>
 
-        
+          <h4 className="text-white mb-3">Trailers</h4>
+          <div className="videos d-sm-flex">
+            {data.videos.results && data.videos.results.map(video => (
+              <Iframer video={video} key={video.key} />
+            ))}
+          </div>
+        </div>
+
       </div>
     ) : (
       <div className="container">
-        { errors ?  <ErRors err={errors} /> : <Loading />} 
+        { errors ? <ErRors err={errors} /> : <Loading />}
       </div>
     );
 
@@ -71,7 +69,17 @@ class detailsPage extends Component {
 const mapStateToProps = state => ({
   movie: state.movies.movie,
   movies: state.movies.movies,
-  errors: state.error.err
+  errors: state.error.err,
 });
+
+detailsPage.propTypes = {
+  match: PropTypes.shape.isRequired,
+  movie: PropTypes.shape.isRequired,
+  movies: PropTypes.shape.isRequired,
+  fetchMovies: PropTypes.func.isRequired,
+  fetchMovie: PropTypes.func.isRequired,
+  errors: PropTypes.shape.isRequired,
+  history: PropTypes.shape.isRequired,
+};
 
 export default connect(mapStateToProps, { fetchMovie, fetchMovies })(detailsPage);
